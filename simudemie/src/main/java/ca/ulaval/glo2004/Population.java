e12c7f6e6891e89d93c0a3e17208c0fc44acdc60
sean wilson <sean0669@gmail.com>
2021-03-20 17:07:47 -0400

    WorldDrawer, Population

---
+++ simudemie/src/main/java/ca/ulaval/glo2004/Population.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.ulaval.glo2004;
+
+/**
+ *
+ * @author Sean
+ */
+public class Population {
+    //    Attributs
+    private int totalPopulation;
+    private int infectedPopulation;
+    private int curedPopulation;
+    private int deadPopulation;
+    private int nonInfectedPopulation;
+    
+    //   methodes
+    private void Population(int p_TotalPop){
+        totalPopulation = p_TotalPop;
+    }
+    
+    public int getTotalPopulation(){
+        return totalPopulation;
+    }
+    
+    public void setTotalPopulation(int p_TotalPop){
+        totalPopulation = p_TotalPop;
+    }
+    
+    public void addPatientZero(){
+        infectedPopulation = 1;
+    }
+    
+    public int getInfectedPopulation(){
+        return infectedPopulation;
+    }
+    
+    public int getCuredPopulation(){
+        return curedPopulation;
+    }
+    
+    public int getDeadPopulation(){
+        return deadPopulation;
+    }
+    
+    public void setInfectedPopulation(int p_InfectedPop){
+        infectedPopulation = p_InfectedPop;
+    }
+    
+    public void setCuredPopulation(int p_CuredPop){
+        curedPopulation = p_CuredPop;
+    }
+    
+    public void setDeadPopulation(int p_DeadPop){
+        deadPopulation = p_DeadPop;
+    }
+    
+    public int getNonInfectedPopulation(){
+        return nonInfectedPopulation;
+    }
+    
+    public void setNonInfectedPopulation(int p_NonInfectedPop){
+        nonInfectedPopulation = p_NonInfectedPop;
+    }
+    
+}
