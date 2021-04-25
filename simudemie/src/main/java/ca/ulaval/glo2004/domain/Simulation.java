e488edb5cf51cab29fa5372479d43afc6819dbe4
Abergel Clement <clement.abergel.1@ulaval.ca>
2021-04-25 11:24:51 -0400

    Fix load

--- simudemie/src/main/java/ca/ulaval/glo2004/domain/Simulation.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/domain/Simulation.java
@@ -56,6 +56,8 @@ public class Simulation implements Serializable {
         controller = p_controller;
         diseaseList = diseases;
         this.elapsedDay = elapsedDay;
+        
+        setcurrentDisease(currentDiseaseIndex);
     }
     
     public boolean HasDisease() {
